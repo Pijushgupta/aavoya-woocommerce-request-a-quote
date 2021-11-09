24d6db73e4b8e63d16ae3e175f6cc57e72dc008f
Pijush Gupta <pijush@live.com>
2021-11-09 16:54:16 +0530

    2nd phase

---
+++ admin/src/setting.js
@@ -0,0 +1,61 @@
+(function ($) {
+
+	/**
+	 * @desc - even driven function to save the settings
+	 */
+	jQuery(".save-settings").on("click", function () {
+		let mainDiv = jQuery(this).parent().parent();
+
+		const globalSettingData = {
+									globalCorner: mainDiv.find("#global-corner").val(),
+									globalText: mainDiv.find("#global-b-text").val(),
+									globalPaddingX: mainDiv.find("#global-padding-x").val(),
+									globalPaddingY: mainDiv.find("#global-padding-y").val(),
+									globalSize: mainDiv.find("#global-size").val(),
+									globalTracking: mainDiv.find("#global-tracking").val(),
+									globalBgColor: mainDiv.find("#global-background-color").val(),
+									globalTextColor: mainDiv.find("#global-text-color").val(),
+									globalCssClass: mainDiv.find("#global-css-class").val()
+		};
+		
+		jQuery.ajax({
+			url: ajaxurl,
+			type: "POST",
+			dataType: "json",
+			data: {
+				action: "save_global_setting",
+				nonce: jQuery("#awraqnonce").val(),
+				globaldata: globalSettingData
+			},
+			error: function (xhr, textStatus, errorThrown) { console.log(errorThrown); },
+			success: function (response) {
+				console.log(response);
+				}
+
+		});
+	
+	});
+
+
+	/**
+	 * @desc event driven - On page load it load global settings data
+	 */
+	jQuery().ready(function () {
+		
+		jQuery.ajax({
+			url: ajaxurl,
+			type: "POST",
+			dataType: "json",
+			data: {
+				action: "get_global_setting",
+				nonce: jQuery("#awraqnonce").val()
+			},
+			error: function (xhr, textStatus, errorThrown) { console.log(errorThrown) },
+			success: function (response) {
+				
+				console.log(response);
+			}
+		});
+	});
+
+})(jQuery);
---
+++ includes/aavoya_initdata.php
@@ -0,0 +1,31 @@
+<?php
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+/**
+ * adding default data(option - css) incase its not present in database.
+ */
+if (get_option('aavoya_wraq_global_settings', false) == false) {
+	update_option('aavoya_wraq_global_settings', createDefaultData());
+}
+
+
+/**
+ * preparing default data
+ */
+function createDefaultData()
+{
+
+	$globaldata['globalBgColor'] 	= '#1e3a8a';
+	$globaldata['globalTextColor'] 	= '#ffffff';
+	$globaldata['globalCorner'] 	= 8;
+	$globaldata['globalPaddingX'] 	= 30;
+	$globaldata['globalPaddingY'] 	= 10;
+	$globaldata['globalSize'] 		= 21;
+	$globaldata['globalTracking'] 	= 6;
+	$globaldata['globalText'] 		= 'Button';
+	$globaldata['globalCssClass'] 	= 'aavoya-wraq-btn';
+
+	return serialize($globaldata);
+}
