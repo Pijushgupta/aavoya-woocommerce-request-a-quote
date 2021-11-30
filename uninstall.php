635d036af36a959ee259056dade21990bd3d791c
Pijush Gupta <pijush@live.com>
2021-12-01 02:11:15 +0530

    simplified uninstall and added an extra checking before registering post type and deleted the class for uninstalling plugin

--- uninstall.php
+++ uninstall.php
@@ -4,46 +4,32 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
-
-if (!class_exists('base')) {
-	require_once plugin_dir_path(__FILE__) . 'includes/base.php';
-}
-
-class aavoya_do_uninstall extends base{
-
-	public function __construct(){
-		$this->aavoya_delete_init();
-	}
-
-	public function aavoya_delete_init(){
-		
-		/**
-		 * creating object with the help load method 
-		 */
-		$avoya_wraq_unistall_steps = $this->load('aavoya_wraquninstall');
-
-		/**
-		 * Deleting all posts(Buttons)
-		 */
-		$avoya_wraq_unistall_steps->delete_aavoya_wraq_posts();
-
-		/**
-		 * removing button post type 
-		 */
-		$avoya_wraq_unistall_steps->delete_aavoya_wraq_post_type();
-		
-		/**
-		 * removing global setting data(button styling)
-		 */
-		$avoya_wraq_unistall_steps->delete_aavoya_global_settings();
-
-		
-	}
-
-
-
-}
 /**
- * create a object of above class to invoke the constructor function 
+ * below array of arguments is used to get all shortcodes
+ * from custom post type of "aavoya_wraq"
  */
-$aavoya_do_uninstall = new aavoya_do_uninstall();
+$argument = array(
+	'post_type' => 'aavoya_wraq',
+	'post_per_page' => -1,
+	'numberposts' => -1
+
+);
+
+/**
+ * getting all the buttons(posts)
+ */
+$shortcodes = get_posts($argument);
+
+/**
+ * deleting buttons
+ */
+foreach ($shortcodes as $shortcode) {
+	wp_delete_post($shortcode->ID, true);
+}
+
+
+unregister_post_type('aavoya_wraq');
+
+delete_option('aavoya_wraq_global_settings');
+
+
