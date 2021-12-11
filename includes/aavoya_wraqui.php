a1f1e28621458d72edec42f94a577d3880c78cab
Pijush Gupta <pijush@live.com>
2021-12-11 23:32:44 +0530

    minor fix

--- includes/aavoya_wraqui.php
+++ includes/aavoya_wraqui.php
@@ -149,7 +149,7 @@ class aavoya_wraqui extends base
 					<path d="M204.288 0C91.648 0 0 91.648 0 204.288s91.648 204.288 204.288 204.288 204.288-91.648 204.288-204.288S316.928 0 204.288 0zm114.176 150.528l-130.56 129.536c-7.68 7.68-19.968 8.192-28.16.512L90.624 217.6c-8.192-7.68-8.704-20.48-1.536-28.672 7.68-8.192 20.48-8.704 28.672-1.024l54.784 50.176L289.28 121.344c8.192-8.192 20.992-8.192 29.184 0s8.192 20.992 0 29.184z" />
 				</svg>
 			</div>
-			<div class="text-xs tracking-wide font-medium ">My Sister/Brother from another mother please <a href="" class="hyperlink">wear a mask</a> and Help Fight COVID-19</div>
+			<div class="text-xs tracking-wide font-medium ">My Sisters/Brothers from another mother please <a href="" class="hyperlink">wear a mask</a> and Help Fight COVID-19</div>
 
 			<div class="ml-auto">
 				<svg xmlns="http://www.w3.org/2000/svg" class="no-msg fill-current h-5 w-5 text-blue-900 cursor-pointer" viewBox="0 0 20 20">
